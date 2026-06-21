e3f75f1329e97a092fdfe53176c0efdea6681539
Đạt Nguyễn <nguyendatz567@gmail.com>
2026-06-21 14:19:46 +0700

    update identity user controller

--- identity-service/app/controllers/userController.js
+++ identity-service/app/controllers/userController.js
@@ -19,6 +19,18 @@ exports.deleteUser = async (req, res) => {
   }
 };
 
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select('-password');
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
 exports.listUsers = async (req, res) => {
   try {
     const users = await User.find().select('-password');
