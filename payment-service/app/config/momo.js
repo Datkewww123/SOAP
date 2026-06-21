fd8228483f15ae434167f609c95bda10dec95f1e
Đạt Nguyễn <nguyendatz567@gmail.com>
2026-06-21 14:19:50 +0700

    add payment MoMo config

---
+++ payment-service/app/config/momo.js
@@ -0,0 +1,8 @@
+module.exports = {
+  partnerCode: process.env.MOMO_PARTNER_CODE || 'MOMO',
+  accessKey: process.env.MOMO_ACCESS_KEY || 'F8BBA842ECF85',
+  secretKey: process.env.MOMO_SECRET_KEY || 'K951B6PE1waDMi640xX08PD3vg6EkVlz',
+  endpoint: process.env.MOMO_ENDPOINT || 'https://test-payment.momo.vn/v2/gateway/api/create',
+  redirectUrl: process.env.MOMO_REDIRECT_URL || 'http://localhost:8080/payment/return',
+  ipnUrl: process.env.MOMO_IPN_URL || 'http://payment-service:3004/api/payment/ipn',
+};
